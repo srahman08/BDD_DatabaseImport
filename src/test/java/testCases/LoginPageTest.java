516d7ea95aaea13cdc9b1e4d184891d761463477
srahman08 <srahman08@gmail.com>
2021-02-14 10:57:01 -0600

    first jenkin push

--- src/test/java/testCases/LoginPageTest.java
+++ src/test/java/testCases/LoginPageTest.java
@@ -3,6 +3,7 @@ package testCases;
 import java.sql.SQLException;
 
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -21,14 +22,29 @@ public class LoginPageTest extends TestBase {
 		loginpageObj = PageFactory.initElements(driver, LoginPage.class);
 	}
 
-	@Test
+	@Test(priority = 1)
 	public void loginTest() throws InterruptedException, ClassNotFoundException, SQLException {
 
-		loginpageObj.enterUserName(DatabasePage.getData("username"));
-		loginpageObj.enterPassword(DatabasePage.getData("password"));
+		loginpageObj.enterUserName("demo@techfios.com");
+		Thread.sleep(2000);
+		loginpageObj.enterPassword("abc123");
+		Thread.sleep(2000);
 		loginpageObj.clickSignInButton();
 		Thread.sleep(2000);
 
+	}
+
+	@Test(priority = 2)
+	public void loginpageTitleTest() throws ClassNotFoundException, SQLException, InterruptedException {
+		loginpageObj.enterUserName("demo@techfios.com");
+		Thread.sleep(2000);
+		loginpageObj.enterPassword("abc123");
+		Thread.sleep(2000);
+		loginpageObj.clickSignInButton();
+		Thread.sleep(2000);
+		String expectedTitle = "Dashboard- iBilling";
+		String actualTitle = loginpageObj.getPageTitle();
+		Assert.assertEquals(expectedTitle, actualTitle);
 	}
 
 	@AfterMethod
